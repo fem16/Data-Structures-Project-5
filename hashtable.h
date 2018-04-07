3e79ddcd5a3e66e4fbd0d2d29dfb2264a97d6d20
Femi Adeyemo <nifemiadeyemo@gmail.com>
2018-04-07 02:01:00 -0400

    Wrote Some Implemetation

--- hashtable.h
+++ hashtable.h
@@ -2,7 +2,6 @@
 #define HASHTABLE_H
 #include <iostream>
 #include <utility>
-#include <pair>
 #include <algorithm>
 #include <vector>
 #include <list>
@@ -16,30 +15,32 @@ static const unsigned int default_capacity = 11;
 namespace cop4530{
 	
 		template <typename K, typename V>
-			class HashTable
+			class HashTable<K, V>
 			{
 				private:
 					void makeEmpty(); ///delete all elements in the hash table. The public interface clear() will call this function
 					void rehash(); //Rehash function. Called when the number of elements in the hash table is greater than the size of the vector.
 					size_t myhash(const K &k); //return the index of the vector entry where k should be stored.
-					unsigned long prime_below (unsigned long);
+					unsigned long prime_below (unsigned long); //Helper to determine prime numbers
 					void setPrimes(vector<unsigned long>&);
 					
+					int size;
+					std::vector<std::list<std::pair<K,V>> theTable;
 					
 					
 				public:
-					HashTable(size_t size = 101);
-					~HashTable();
-					bool contains(const K & k); 
-					bool match(const std::pair<K, V> &kv) const;
-					bool insert(const std::pair<K, V> & kv); 
-					bool insert (std::pair<K,  V> && kv); 
-					bool remove(const K & k);
-					void clear(); 
-					bool load(const char *filename); 
-					void dump(); 
-					size_t size(); 
-					bool write_to_file(const char *filename);
+					HashTable(size_t theSize = 101);//Constructor
+					~HashTable(); //Destructor
+					bool contains(const K & k); //Check if key is in hashtable
+					bool match(const std::pair<K, V> &kv) const; //Ckeck if pair is in hash table
+					bool insert(const std::pair<K, V> & kv); //Add key-value pair into hash table
+					bool insert (std::pair<K, V> && kv); //Move version of insert
+					bool remove(const K & k);	//Delete k and value from hash table
+					void clear(); //Delete all elements in hash table
+					bool load(const char *filename); //Load the content of the file into hash table
+					void dump(); //Display all entries in the hash table
+					size_t size(); //Return the number of elements in the hash table
+					bool write_to_file(const char *filename); //Write all elements in the hash table into file with filename
 										
 			};//end of class
 			
--- hashtable.hpp
+++ hashtable.hpp
@@ -54,3 +54,168 @@ void HashTable<K, V>::setPrimes(std::vector<unsigned long>& vprimes)
     }
 }
 
+//delete all elements in the hash table. The public interface clear() will call this function
+template <typename K, typename V>
+void HashTable<K, V>::makeEmpty() 
+{
+	clear();
+}
+
+//Rehash function. Called when the number of elements in the hash table is greater than the size of the vector.
+template <typename K, typename V>
+void HashTable<K, V>::rehash()
+{
+	vector <std::pair<K,V>> prevList = theTable;
+	
+	theTable.resize(prime_below(2 * prevList.size()));
+	
+	size = 0;
+	
+	for(auto &pair : prevList)
+		insert(std::move(pair));
+}
+//return the index of the vector entry where k should be stored.
+template <typename K, typename V>
+size_t HashTable<K, V>::myhash(const K &k)
+{
+	return k % theTable.size();
+}
+
+//Constructor
+template <typename K, typename V>
+HashTable<K, V>::HashTable(size_t theSize = 101)
+{
+	size = prime_below(theSize);
+	
+}
+
+//Destructor
+template <typename K, typename V>
+HashTable<K, V>::~HashTable() 
+{
+	clear();
+}
+
+//Check if key is in hashtable
+template <typename K, typename V>
+bool HashTable<K, V>::contains(const K & k)
+{
+	for(auto list: theTable)
+	{
+		if(std::find_if(begin(list), end(list), [](const std::pair<K, V> p)
+		{return p.first == kv.first;} ) != end(list));
+			return true;
+	}
+	
+	return false;
+}
+ 
+ //Ckeck if pair is in hash table
+template <typename K, typename V>
+bool HashTable<K, V>::match(const std::pair<K, V> &kv) const
+{
+	for(auto list: theTable)
+	{
+		if(std::find_if(begin(list), end(list), [](const std::pair<K, V> p)
+		{return p.first == kv.first && p.second == kv.second;} ) != end(list));
+			return true;
+	}
+	
+	return false;
+}
+
+//Add key-value pair into hash table
+template <typename K, typename V>
+bool HashTable<K, V>::insert(const std::pair<K, V> & kv) 
+{
+	
+	auto & theList = theTable[myhash(kv.first)];
+	
+	size++;
+	
+	if(match(kv))
+		return false;
+	
+	if(contains(kv.first))
+	{
+		for(auto & p : theList)
+		{
+			if(p.first == kv.first)
+			{
+					p.second = kv.second;
+			}
+				
+		}
+	}
+	else 
+		theList.push_back(kv);
+	
+	if(++size > theTable.size())
+		rehash();
+	
+	return true;
+}
+
+//Move version of insert
+template <typename K, typename V>
+bool HashTable<K, V>::insert(std::pair<K, V> && kv)
+{
+	auto & theList = theTable[myhash(kv.first)];
+	size++;
+	
+	if(match(kv))
+		return false;
+	
+	if(contains(kv.first))
+	{
+		for(auto & p : theList)
+		{
+			if(p.first == kv.first)
+			{
+					p.second = std::move(kv.second);
+			}
+				
+		}
+	}
+	else 
+		theList.push_back(kv);
+	
+	if(++size > theTable.size())
+		rehash();
+	
+	return true;
+}
+//Delete k and value from hash table
+template <typename K, typename V>
+bool HashTable<K, V>::remove(const K & k)
+{
+	auto theList = theTable[myhash(k)];
+	
+	auto itr = std::find_if(begin(theList), end(theList), [](const std::pair<K, V> p)
+		{return p.first == kv.first && p.second == kv.second;} );
+	
+	if(itr == end(theList))
+		return false;
+	
+	theList.erase(itr);
+	--size;
+	return true;
+}
+
+//Delete all elements in hash table
+template <typename K, typename V>
+void clear()
+{
+	for(auto & list : theTable)
+	{
+		list.clear();
+	}
+	
+	size = 0;
+	
+}
+bool load(const char *filename); //Load the content of the file into hash table
+void dump(); //Display all entries in the hash table
+size_t size(); //Return the number of elements in the hash table
+bool write_to_file(const char *filename); //Write all elements in the hash table into file with filename
+
